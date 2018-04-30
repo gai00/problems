8f57d41e97b6b5470762ac4093a432258f70a74f
Layer <gai00@users.noreply.github.com>
2018-04-30 13:08:42 +0800

    Create main.js

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.body.addEventListener('load', (e) => {
+  const app = document.getElementById('app');
+  console.log(app);
+});
